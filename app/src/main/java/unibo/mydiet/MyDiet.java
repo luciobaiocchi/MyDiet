957a2c1456c2a3076b10627b2d0959b44322face
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2024-07-15 14:25:38 +0200

    removed gradle

---
+++ app/src/main/java/unibo/mydiet/MyDiet.java
@@ -0,0 +1,20 @@
+package unibo.mydiet;
+
+
+/**
+ * The main application loader.
+ */
+public final class MyDiet {
+    private MyDiet() {
+
+    }
+
+    /**
+     * Starts the login view.
+     *
+     * @param args The arguments passed to the application.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
