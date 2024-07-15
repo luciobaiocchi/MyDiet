ef2cfe50509589c987a89f340a151887fe83f4fa
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2024-07-15 14:49:19 +0200

    added gradle

---
+++ src/main/java/mydiet/application/MyDiet.java
@@ -0,0 +1,20 @@
+package gymlife.application;
+
+
+/**
+ * The main application loader.
+ */
+public final class MyDiet {
+    private MyDiet() {
+
+    }
+
+    /**
+     * Starts the login view.
+     *
+     * @param args The arguments passed to the application.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
